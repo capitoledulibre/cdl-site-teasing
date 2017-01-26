ee75d84e1d3c15f95e046dddaeb8d9a77493857d
numahell <numahell@numajules.net>
2017-01-27 00:59:45 +0100

    Video by track. Fix some troubles.

--- www/js/videos.js
+++ www/js/videos.js
@@ -17,64 +17,87 @@
     }
 
     function fetchProgram(callback) {
-      var xhr = new XMLHttpRequest();
-      xhr.onload = function() {
-        callback(xhr.responseXML);
-      }
-      xhr.onerror = function() {
-        console.error("Error while getting program XML.", xhr.status, xhr.statusText);
-      }
-      xhr.open("GET", config.programmeUrl);
-      xhr.responseType = "document";
-      xhr.send();
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+          callback(xhr.responseXML);
+        }
+        xhr.onerror = function() {
+          console.error("Error while getting program XML.", xhr.status, xhr.statusText);
+        }
+        xhr.open("GET", config.programmeUrl);
+        xhr.responseType = "document";
+        xhr.send();
+    }
+
+    function renderTrackDiv(track, index) {
+        var html = '';
+        html = '<section id="track-' + index + '">' +
+            '<header class="col-xs-12">' +
+            '<h3>' + track + '</h3>'
+            '</header>' +
+            '</section>';
+        return html;
+    }
+
+
+    function renderVideoEvent(event, index) {
+        var html = '';
+        html = '<div class="col-xs-12 col-sm-6 col-md-4">' +
+            '<div class="coverage media-video">' +
+            '<header>' +
+            '<h4>' + event.title + '</h4>' +
+            '<small>' + event.persons.join(', ') + '</small>' +
+            '</header>' +
+            '<video width="360" height="200" controls="controls" preload="none">' +
+            '<source src="' +
+            event.video +
+            '" type="video/mp4"/>' +
+            '</video>' +
+            '</div>' +
+            '</div>';
+        if ((index + 1) % 3 === 0) {
+            html += '<div class="clearfix"></div>';
+        }
+        return html;
     }
 
     function listVideos(programXML) {
 
-      var events = []
-      var eventElems = Array.prototype.slice.call(programXML.getElementsByTagName('event'));
+      var eventsByTrack = {};
+      var eventElems = programXML.getElementsByTagName('event');
 
-      eventElems.forEach(function(eventElem) {
-            var event = {
+      Array.forEach(eventElems, function(eventElem) {
+          var event = {
               id: parseInt(eventElem.getAttribute('id')),
               slug: getChildText(eventElem, 'slug'),
               title: getChildText(eventElem, 'title'),
               track: getChildText(eventElem, 'track'),
               type: getChildText(eventElem, 'type'),
-            };
-            event.persons = Array.prototype.slice.call(eventElem.getElementsByTagName('person')).map(function(personElem) {
+          };
+          event.persons = Array.map(eventElem.getElementsByTagName('person'), function(personElem) {
               return personElem.textContent
-            });
-            event.video = Array.prototype.slice.call(eventElem.getElementsByTagName('link')).map(function(linkElem) {
+          });
+          Array.forEach(eventElem.getElementsByTagName('link'), function(linkElem) {
+              // Just need video
               if (linkElem.getAttribute('tag') === 'video') {
-                return linkElem.textContent;
+                  event.video = linkElem.textContent;
+                  if (!(event.track in eventsByTrack)) {
+                    eventsByTrack[event.track] = [];
+                  }
+                  eventsByTrack[event.track].push(event);
               }
-            });
-            if ('video' in event) {
-                events.push(event);
-            }
-
-      })
-
-      var html = '';
-      events.forEach( function(event) {
-        var item = '';
-        item = '<div class="col-xs-12 col-sm-6 col-md-4">' +
-        '<div class="coverage media-video">' +
-        '<header>' +
-        '<h4>' + event.title + '</h4>' +
-        '<small>' + event.persons.join(', ') + '</small>' +
-        '</header>' +
-        '<video width="360" height="200" controls="controls" preload="none">' +
-        '<source src="' +
-        event.videoLink +
-        '" type="video/mp4"/>' +
-        '</video>' +
-        '</div>' +
-        '</div>';
-        html += item;
+          });
       });
-      $('.videos').append(html);
+
+      // Display for each track its events
+      var trackId = 0;
+      for (var track in eventsByTrack) {
+          trackId ++;
+          $('.videos').append(renderTrackDiv (track, trackId));
+          eventsByTrack[track].forEach(function (event, index) {
+              $('.videos #track-' + trackId).append(renderVideoEvent (event, index));
+          });
+      }
     }
 
     function init() {
